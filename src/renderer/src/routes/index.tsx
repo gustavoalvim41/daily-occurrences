6f191e78b1b5b659836f1e64ac74db31a782578e
gustavoalvim41 <gustavoalvim85@gmail.com>
2023-05-13 16:44:38 -0300

    adding occorrences screen the routes

--- src/renderer/src/routes/index.tsx
+++ src/renderer/src/routes/index.tsx
@@ -4,6 +4,7 @@ import Layout from '../components/Layout'
 
 import Home from '../screens/Home'
 import Settings from '../screens/Settings'
+import Occurrences from '@renderer/screens/Occurrences'
 
 function AppRouter(): JSX.Element {
   return (
@@ -12,6 +13,7 @@ function AppRouter(): JSX.Element {
         <Routes>
           <Route element={<Home />} path="/" />
           <Route element={<Settings />} path="/settings" />
+          <Route element={<Occurrences />} path="/occurrences" />
         </Routes>
       </Layout>
     </Router>
