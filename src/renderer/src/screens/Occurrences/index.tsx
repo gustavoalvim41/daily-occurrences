cf620a6f271148e5913758b1f7afff08b3ba2ec1
gustavoalvim41 <gustavoalvim85@gmail.com>
2023-05-13 16:42:04 -0300

    adding occurrences screen

---
+++ src/renderer/src/screens/Occurrences/index.tsx
@@ -0,0 +1,11 @@
+import './styles.sass'
+
+function Occurrences(): JSX.Element {
+  return (
+    <div className="occurrences">
+      <p>Occurrences Screen</p>
+    </div>
+  )
+}
+
+export default Occurrences
