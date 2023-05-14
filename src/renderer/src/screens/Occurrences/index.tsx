7c5b10250d6d318c793879386f950ae59fc51268
gustavoalvim41 <gustavoalvim85@gmail.com>
2023-05-14 16:33:30 -0300

    adding table in occurrences screen

--- src/renderer/src/screens/Occurrences/index.tsx
+++ src/renderer/src/screens/Occurrences/index.tsx
@@ -4,10 +4,50 @@ function Occurrences(): JSX.Element {
   return (
     <div className="occurrences">
       <div className="head">
-        <button>New Occurrences</button>
+        <a>New Occurrences</a>
       </div>
       <div className="content">
-        <p>Occurrences Screen</p>
+        <table>
+          <thead>
+            <tr>
+              <th>date</th>
+              <th>team</th>
+              <th>type</th>
+              <th>address</th>
+              <th>status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>2023/05/14</td>
+              <td>Person 1 And Person 2</td>
+              <td>Animal At Risk</td>
+              <td>New York, New York(NY), 10003</td>
+              <td>Finished</td>
+            </tr>
+            <tr>
+              <td>2023/05/14</td>
+              <td>Person 1 And Person 2</td>
+              <td>Animal At Risk</td>
+              <td>New York, New York(NY), 10003</td>
+              <td>Finished</td>
+            </tr>
+            <tr>
+              <td>2023/05/14</td>
+              <td>Person 1 And Person 2</td>
+              <td>Animal At Risk</td>
+              <td>New York, New York(NY), 10003</td>
+              <td>Finished</td>
+            </tr>
+            <tr>
+              <td>2023/05/14</td>
+              <td>Person 1 And Person 2</td>
+              <td>Animal At Risk</td>
+              <td>New York, New York(NY), 10003</td>
+              <td>Finished</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   )
