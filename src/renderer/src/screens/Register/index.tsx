9c9c8bd2db9dea308d2eb611f28b5e9188c7eae7
gustavoalvim41 <gustavoalvim85@gmail.com>
2023-05-15 21:51:54 -0300

    Add registration component with date and time fields

--- src/renderer/src/screens/Register/index.tsx
+++ src/renderer/src/screens/Register/index.tsx
@@ -1,9 +1,50 @@
+import { useState } from 'react'
 import './styles.sass'
+import { format } from 'date-fns'
 
 function Register(): JSX.Element {
+  const [selectedDate, setSelectedDate] = useState('')
+  const [selectedTime, setSelectedTime] = useState('')
+  const [formattedDateTime, setFormattedDateTime] = useState('')
+
+  const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setSelectedDate(event.target.value)
+    formatDateTime(event.target.value, selectedTime)
+  }
+
+  const handleTimeChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setSelectedTime(event.target.value)
+    formatDateTime(selectedDate, event.target.value)
+  }
+
+  const formatDateTime = (date: string, time: string): void => {
+    if (date && time) {
+      const dateTime = new Date(`${date}T${time}`)
+      const formatted = format(dateTime, 'dd/MM/yyyy - HH:mm')
+      setFormattedDateTime(formatted)
+    } else {
+      setFormattedDateTime('')
+    }
+  }
+
+  const handleRegister = (): void => {
+    // Lógica para registrar os dados no banco de dados
+    console.log(formattedDateTime)
+  }
+
   return (
     <div className="register">
-      <p>Register Screen</p>
+      <div className="form">
+        <div className="row">
+          <input type="date" value={selectedDate} onChange={handleDateChange} />
+        </div>
+        <div className="row">
+          <input type="time" value={selectedTime} onChange={handleTimeChange} />
+        </div>
+        <div className="row">
+          <button onClick={handleRegister}>register</button>
+        </div>
+      </div>
     </div>
   )
 }
