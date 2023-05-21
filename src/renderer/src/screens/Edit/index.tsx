caf2f533a2ec4b7bcc1fdb28a49368940d3e8763
gustavoalvim41 <gustavoalvim85@gmail.com>
2023-05-21 01:36:41 -0300

    Add fetching and rendering of occurrence data.

--- src/renderer/src/screens/Edit/index.tsx
+++ src/renderer/src/screens/Edit/index.tsx
@@ -1,12 +1,53 @@
-import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 import './styles.sass'
+import { useParams } from 'react-router-dom'
+import db from '../../database'
+
+type OccurrenceProps = {
+  id?: number
+  date: string
+  time: string
+  participantOne: string
+  participantTwo: string
+  incident: string
+  address: string
+  receipt: string
+  situation: string
+  observation: string
+}
 
 function Edit(): JSX.Element {
-  const { id } = useParams()
+  const { id } = useParams<{ id: string }>()
+
+  const [occurrence, setOccurrence] = useState<OccurrenceProps[]>([])
+
+  useEffect(() => {
+    const fetchItem = async (): Promise<void> => {
+      try {
+        if (id) {
+          const foundItem = await db.occurrences.get(+id)
+          if (foundItem) {
+            setOccurrence([foundItem])
+          }
+        }
+      } catch (error) {
+        console.log('Erro ao buscar o item:', error)
+      }
+    }
+
+    fetchItem()
+  }, [id])
 
   return (
     <div className="edit">
-      <p>Seu ID: {id}</p>
+      {occurrence?.map((occurrence) => (
+        <div key={occurrence.id}>
+          <p>{occurrence.date + ' ' + occurrence.time}</p>
+          <p>{occurrence.incident}</p>
+          <p>{occurrence.address}</p>
+          <p>{occurrence.situation}</p>
+        </div>
+      ))}
     </div>
   )
 }
